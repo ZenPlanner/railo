ce43131c1add41f5d1c3a0a423f536a0984358ce
Brent Gardner <brent.gardner@zenplanner.com>
2014-11-20 05:55:24 -0700

    Able to complete a run actually hitting all tags

--- parse-test/src/com/zenplanner/StatementScanner.java
+++ parse-test/src/com/zenplanner/StatementScanner.java
@@ -3,18 +3,20 @@ package com.zenplanner;
 import railo.transformer.bytecode.Body;
 import railo.transformer.bytecode.Page;
 import railo.transformer.bytecode.Statement;
+import railo.transformer.bytecode.cast.CastBoolean;
+import railo.transformer.bytecode.cast.CastString;
 import railo.transformer.bytecode.expression.Expression;
 import railo.transformer.bytecode.expression.var.*;
-import railo.transformer.bytecode.statement.Condition;
-import railo.transformer.bytecode.statement.ExpressionStatement;
-import railo.transformer.bytecode.statement.HasBody;
-import railo.transformer.bytecode.statement.PrintOut;
+import railo.transformer.bytecode.expression.var.Argument;
+import railo.transformer.bytecode.literal.LitBoolean;
+import railo.transformer.bytecode.literal.LitDouble;
+import railo.transformer.bytecode.literal.LitString;
+import railo.transformer.bytecode.op.*;
+import railo.transformer.bytecode.statement.*;
 import railo.transformer.bytecode.statement.tag.*;
 import railo.transformer.library.tag.TagLibTag;
 
 import java.lang.reflect.Field;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Set;
 
 public class StatementScanner {
@@ -30,56 +32,99 @@ public class StatementScanner {
     }
 
     private void processStatement(Statement stmt) throws Exception {
-        if(stmt instanceof Page) {
-            Page p = (Page)stmt;
+        Class<?> clazz = stmt.getClass();
+        if (clazz == Page.class) {
+            Page p = (Page) stmt;
             processBody(p);
             return;
         }
-        if(stmt instanceof PrintOut) {
-            return; // No recursion
+        if (clazz == PrintOut.class) {
+            PrintOut po = (PrintOut)stmt;
+            processExpression(po.getExpr());
+            return;
         }
-        if (stmt instanceof TagImport) {
+        if (clazz == TagImport.class) {
             TagImport ti = (TagImport) stmt;
             processBody(ti.getBody());
             return;
         }
-        if (stmt instanceof TagInclude) {
-            TagInclude ti = (TagInclude)stmt;
+        if (clazz == TagInclude.class) {
+            TagInclude ti = (TagInclude) stmt;
             Attribute template = ti.getAttribute("template");
             references.add(template.getValue().toString().toLowerCase());
             processBody(ti.getBody());
             return;
         }
-        if (stmt instanceof TagIf) {
-            TagIf ti = (TagIf)stmt;
+        if (clazz == TagIf.class) {
+            TagIf ti = (TagIf) stmt;
             processBody(ti.getBody());
             return;
         }
-        if (stmt instanceof TagOutput) {
-            TagOutput ti = (TagOutput)stmt;
+        if (clazz == TagOutput.class) {
+            TagOutput ti = (TagOutput) stmt;
             processBody(ti.getBody());
             return;
         }
-        if (stmt instanceof TagParam) {
-            TagParam tp = (TagParam)stmt;
+        if (clazz == TagParam.class) {
+            TagParam tp = (TagParam) stmt;
             processBody(tp.getBody());
             return;
         }
-        if (stmt instanceof ExpressionStatement) {
-            ExpressionStatement es = (ExpressionStatement)stmt;
+        if (clazz == ExpressionStatement.class) {
+            ExpressionStatement es = (ExpressionStatement) stmt;
             Field f = ExpressionStatement.class.getDeclaredField("expr");
             f.setAccessible(true);
-            Expression exp = (Expression)f.get(es);
+            Expression exp = (Expression) f.get(es);
             processExpression(exp);
             return;
         }
-        if(stmt instanceof Tag) {
-            processTag((Tag)stmt);
+        if (clazz == Condition.class) {
+            Condition con = (Condition) stmt;
+            for (Body body : con.getBodies()) {
+                processBody(body);
+            }
             return;
         }
-        if(stmt instanceof Condition) {
-            Condition con = (Condition)stmt;
-            for(Body body : con.getBodies()) {
+        if(clazz == TagComponent.class) {
+            TagComponent comp = (TagComponent)stmt;
+            Attribute attr = comp.getAttribute("extends");
+            if(attr == null) {
+                return;
+            }
+            String superClass = attr.getValue().toString().toLowerCase();
+            references.add(superClass);
+            processBody(comp.getBody());
+            return;
+        }
+        if (clazz == Tag.class) {
+            Tag tag = (Tag)stmt;
+            processTag(tag);
+            processBody(tag.getBody());
+            return;
+        }
+        if(clazz == Switch.class) {
+            Switch sw = (Switch)stmt;
+            for(Body body : sw.getBodies()) {
+                processBody(body);
+            }
+            return;
+        }
+        if (stmt instanceof Body) {
+            Body tag = (Body)stmt;
+            processBody(tag);
+            return;
+        }
+        if (stmt instanceof HasBody) {
+            HasBody tag = (HasBody)stmt;
+            processBody(tag.getBody());
+            return;
+        }
+        if(stmt instanceof Return) {
+            return;
+        }
+        if(stmt instanceof TryCatchFinally) {
+            TryCatchFinally tcf = (TryCatchFinally)stmt;
+            for(Body body : tcf.getBodies()) {
                 processBody(body);
             }
             return;
@@ -88,51 +133,122 @@ public class StatementScanner {
     }
 
     private void processBody(Body body) throws Exception {
-        if(body == null) {
+        if (body == null) {
             return;
         }
-        for(Object obj : body.getStatements()) {
-            Statement stmt = (Statement)obj;
+        for (Object obj : body.getStatements()) {
+            Statement stmt = (Statement) obj;
             processStatement(stmt);
         }
     }
 
-    private void processExpression(Expression exp) {
-        if(exp instanceof Assign) {
-            Assign ass = (Assign)exp;
+    private void processExpression(Expression exp) throws Exception {
+        Class<?> clazz = exp.getClass();
+        if (clazz == Assign.class || clazz == OpVariable.class) {
+            Assign ass = (Assign) exp;
             Expression child = ass.getValue();
             processExpression(child);
             return;
         }
-        if(exp instanceof Variable) {
-            Variable var = (Variable)exp;
-            for(Object obj : var.getMembers()) {
+        if (clazz == Variable.class) {
+            Variable var = (Variable) exp;
+            for (Object obj : var.getMembers()) {
                 processMember((Member) obj);
             }
+            return;
+        }
+        if(clazz == OpContional.class) {
+            OpContional op = (OpContional)exp;
+            Field cont = OpContional.class.getDeclaredField("cont");
+            cont.setAccessible(true);
+            processExpression((Expression)cont.get(op));
+
+            processLAndR(exp);
+
+            return;
+        }
+        if(clazz == OPDecision.class) {
+            processLAndR(exp);
+            return;
+        }
+        if(clazz == OpString.class) {
+            processLAndR(exp);
+            return;
+        }
+        if(clazz == OpDouble.class) {
+            processLAndR(exp);
+            return;
+        }
+        if(clazz == OpBool.class) {
+            processLAndR(exp);
+            return;
+        }
+        if(clazz == CastString.class) {
+            CastString cast = (CastString)exp;
+            processExpression(cast.getExpr());
+            return;
+        }
+        if(clazz == OpNegate.class) {
+            Field expr = exp.getClass().getDeclaredField("expr");
+            expr.setAccessible(true);
+            processExpression((Expression)expr.get(exp));
+            return;
+        }
+        if(clazz == OpNegateNumber.class) {
+            Field expr = exp.getClass().getDeclaredField("expr");
+            expr.setAccessible(true);
+            processExpression((Expression)expr.get(exp));
+            return;
+        }
+        if(clazz == CastBoolean.class) {
+            Field expr = exp.getClass().getDeclaredField("expr");
+            expr.setAccessible(true);
+            processExpression((Expression)expr.get(exp));
+            return;
+        }
+        if(clazz == LitString.class) {
+            return;
+        }
+        if(clazz == LitBoolean.class) {
+            return;
+        }
+        if(clazz == LitDouble.class) {
             return;
         }
         throw new RuntimeException("Unknown Expression: " + exp);
     }
 
+    private void processLAndR(Object op) throws Exception {
+        Field left = op.getClass().getDeclaredField("left");
+        left.setAccessible(true);
+        processExpression((Expression)left.get(op));
+
+        Field right = op.getClass().getDeclaredField("right");
+        right.setAccessible(true);
+        processExpression((Expression)right.get(op));
+    }
+
     private void processMember(Member mem) {
-        if(mem instanceof BIF) {
-            BIF bif = (BIF)mem;
+        Class<?> clazz = mem.getClass();
+        if (clazz == BIF.class) {
+            BIF bif = (BIF) mem;
             String className = bif.getClassName();
-            if("railo.runtime.functions.other.CreateObject".equalsIgnoreCase(className)) {
+            if ("railo.runtime.functions.other.CreateObject".equalsIgnoreCase(className)) {
                 Argument[] args = bif.getArguments();
                 String arg0 = args[0].getValue().toString();
-                if(!"component".equalsIgnoreCase(arg0)) {
+                if (!"component".equalsIgnoreCase(arg0)) {
                     return;
                 }
-                String arg1 = args[0].getValue().toString();
+                String arg1 = args[1].getValue().toString().toLowerCase();
+                references.add(arg1);
                 return;
             }
             return;
         }
-        if(mem instanceof DataMember) {
+        if (clazz == DataMember.class) {
             return;
         }
-        if(mem instanceof UDF) {
+        if (clazz == UDF.class) {
             return;
         }
         throw new RuntimeException("Unknown Member: " + mem);
@@ -140,22 +256,23 @@ public class StatementScanner {
 
     private void processTag(Tag tag) {
         String name = tag.getFullname();
-        if(name == null) {
+        if (name == null) {
             return; // Null tag hack for pure CFCs
         }
-        if (name.contains(":")) { // TODO: Custom tags
-            String txt = "";
-            for(Object key : tag.getAttributes().keySet()) {
-                txt += "\n\t" + key + "=" + tag.getAttribute((String)key);
-            }
-            TagLibTag tlt = tag.getTagLibTag().getTagLib().getAppendixTag(null);
-            String fileName = tag.getAppendix() + ".cfm";
-            String path = (String)tlt.getAttribute("__custom_tag_path").getDefaultValue() + "/";
-            path += fileName;
-            references.add(path);
-            //System.out.println("CustomTag " + txt);
+        if (!name.contains(":")) {
             return;
         }
+
+        // Custom tags
+        String txt = "";
+        for (Object key : tag.getAttributes().keySet()) {
+            txt += "\n\t" + key + "=" + tag.getAttribute((String) key);
+        }
+        TagLibTag tlt = tag.getTagLibTag().getTagLib().getAppendixTag(null);
+        String fileName = tag.getAppendix() + ".cfm";
+        String path = tlt.getAttribute("__custom_tag_path").getDefaultValue() + "/";
+        path += fileName;
+        references.add(path);
     }
 
 }
